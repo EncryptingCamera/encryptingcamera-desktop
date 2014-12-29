bab9cc034f1b9a97cfeb4867baf2730a2e623793
Taylor Hornby <havoc@defuse.ca>
2014-12-28 19:39:27 -0700

    Use simple (and regular) file format

--- TestEncryption.rb
+++ TestEncryption.rb
@@ -2,12 +2,8 @@
 
 $LOAD_PATH << File.dirname( __FILE__ )
 
-require 'json'
-require 'base64'
 require 'libs/KeyConfiguration.rb'
 require 'libs/Paths.rb'
-
-file = { "version" => nil, "public_key" => nil, "ciphertext" => nil }
 
 ephemeral_private_key = RbNaCl::PrivateKey.generate
 ephemeral_public_key = ephemeral_private_key.public_key
@@ -19,11 +15,8 @@ box = RbNaCl::SimpleBox.from_keypair(destination_public_key, ephemeral_private_k
 
 image_bytes = File.read("./test-image.jpg").force_encoding("BINARY")
 
-file["version"] = 1
-file["public_key"] = Base64.strict_encode64(ephemeral_public_key.to_bytes)
-file["ciphertext"] = Base64.strict_encode64(box.encrypt(image_bytes))
-
-json = JSON.dump(file)
 File.open("test.encc", "w") do |f|
-  f.write(json)
+  f.write("\x01")
+  f.write(ephemeral_public_key.to_bytes)
+  f.write(box.encrypt(image_bytes))
 end
--- libs/DecryptCommand.rb
+++ libs/DecryptCommand.rb
@@ -1,24 +1,30 @@
+# encoding: US-ASCII
 require 'libs/Paths.rb'
 require 'libs/KeyConfiguration.rb'
 require 'json'
 require 'base64'
 
 class DecryptCommand
-  attr_accessor :source, :destination
-  def run
-    json = File.read(@source)
-    parsed = JSON.parse(json)
 
-    if parsed["version"] == 1
+  VER1_REGEXP = Regexp.compile("^(\x01)([\x00-\xFF]{32})([\x00-\xFF]*)$")
+
+  attr_accessor :source, :destination
+
+  def run
+    file_contents = File.read(@source).force_encoding("BINARY")
+
+    match = VER1_REGEXP.match(file_contents)
+
+    if match
+      public_key_bytes = match[2]
+      ciphertext_bytes = match[3]
+      sender_public_key = RbNaCl::PublicKey.new( public_key_bytes )
+
       keyconfig = KeyConfiguration.new( Paths.config_dir )
       our_private_key = keyconfig.get_private_key
 
-      public_key_binary = Base64.strict_decode64( parsed["public_key"] )
-      ciphertext_binary = Base64.strict_decode64( parsed["ciphertext"] )
-
-      sender_public_key = RbNaCl::PublicKey.new( public_key_binary )
       box = RbNaCl::SimpleBox.from_keypair(sender_public_key, our_private_key)
-      plaintext = box.decrypt( ciphertext_binary )
+      plaintext = box.decrypt( ciphertext_bytes )
 
       File.open( @destination, "w") do |f|
         f.write(plaintext)
